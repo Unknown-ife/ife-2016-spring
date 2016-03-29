e84e29648299921a8b4ff7f713a0de99f84d08bd
ahonn <ahonn95@outlook.com>
2016-03-29 14:26:51 +0800

    fix autoprefixer

--- 01/task_07/ahonn/gulpfile.js
+++ 01/task_07/ahonn/gulpfile.js
@@ -6,7 +6,7 @@ var autoprefixer = require('gulp-autoprefixer')
 gulp.task('less', function () {
     gulp.src('./less/style.less')
         .pipe(less())
-        .pipe(autoprefixer({browsers: ['> 1%', 'last 3 versions', 'iOS >= 6', 'android 4']}))
+        .pipe(autoprefixer('last 3 versions'))
         .pipe(gulp.dest('./css'));
 });
 
