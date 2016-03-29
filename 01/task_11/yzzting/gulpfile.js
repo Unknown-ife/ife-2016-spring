63a698442f6555e7cd7f3e9be889240fa0280518
yzzting <yzz1995@aliyun.com>
2016-03-30 00:13:40 +0800

    substantially completed task_11 yzzting

--- 01/task_11/yzzting/gulpfile.js
+++ 01/task_11/yzzting/gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp')
 var less = require('gulp-less')
+var postcss = require('gulp-postcss');
 // var cssmin = require('gulp-minify-css');
-gulp.task('default',function () {
+var autoprefixer = require('autoprefixer');
+gulp.task('default', function() {
     gulp.src(['less/main.less'])
         .pipe(less())
         // .pipe(cssmin())
+        .pipe(postcss([autoprefixer({
+            browsers: ['last 2 versions']
+        })]))
         .pipe(gulp.dest('css'));
 });
-gulp.task('lessWatch',function () {
-    gulp.watch('less/*.less',['default'])
+gulp.task('lessWatch', function() {
+    gulp.watch('less/*.less', ['default'])
 })
